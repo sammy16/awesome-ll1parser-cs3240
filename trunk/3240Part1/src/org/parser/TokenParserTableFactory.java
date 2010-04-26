7188d7879af99ff8a671766bc4cc7161d2eb3f1e
AGuyton <AGuyton@3200653a-81a1-42a6-27f5-1b7c9c2cfb95>
2010-04-26 21:55:12 +0000

    Conversion of string into rules

--- trunk/3240Part1/src/org/parser/TokenParserTableFactory.java
+++ trunk/3240Part1/src/org/parser/TokenParserTableFactory.java
@@ -2,6 +2,7 @@ package org.parser;
 import java.io.*;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.swing.JOptionPane;
 
@@ -87,25 +88,34 @@ public class TokenParserTableFactory {
 	// specifies how to parse the file
 	private void parseFile(BufferedReader buffer)
 	{
-		// read in the tokens and nonterminals and store them appropriately
+		// read in the tokens and store them
 		String tokens = readLine(buffer);
 		ArrayList<Token> tok = new ArrayList<Token>();
 		String[] temp = tokens.split(" ");
-		for(int i=0; i<temp.length; i++)
+		// i=0 is %TOKENS, skip it
+		for(int i=1; i<temp.length; i++)
 		{
-			Token t = new Token();
-			t.TokenParse(temp[i]);
-			tok.add(t);
+			if(!temp[i].trim().equals(""))
+			{
+				Token t = new Token();
+				t.TokenParse(temp[i]);
+				tok.add(t);
+			}
 		}
 		table.setTokens(tok);
 		
+		// read in nonterminals and store them
 		String nonterminals = readLine(buffer);
 		ArrayList<Nonterminal> nont = new ArrayList<Nonterminal>();
 		String[] temp2 = nonterminals.split(" ");
-		for(int i=0; i<temp2.length; i++)
+		// i=0 is %NONTERMINALS, skip it
+		for(int i=1; i<temp2.length; i++)
 		{
-			Nonterminal n = new Nonterminal(temp2[i]);
-			nont.add(n);
+			if(!temp2[i].trim().equals(""))
+			{
+				Nonterminal n = new Nonterminal(temp2[i]);
+				nont.add(n);
+			}
 		}
 		table.setNonterminals(nont);
 		
@@ -128,21 +138,52 @@ public class TokenParserTableFactory {
 		}
 		
 		// fix them by removing left recursion and common prefix
-		ArrayList<String> rules;
+		HashMap<Nonterminal, ArrayList<Rule>> rules;
 		rules = removeLeftRecursion(grammarRules);
 		rules = removeCommonPrefix(rules);
 		
 		// add the rules to the parse table now?
 	}
 	
-	// magical function that removes left recursion
-	private ArrayList<String> removeLeftRecursion(ArrayList<String> rules)
+	// converts a string to a rule that we understand
+	public ArrayList<Rule> getRules(String rawRule)
 	{
+		ArrayList<Rule> result = new ArrayList<Rule>();
+		
+		int colon = rawRule.indexOf(":");
+		String left = rawRule.substring(0, colon).trim();
+		Nonterminal n = new Nonterminal(left);
+		
+		String right = rawRule.substring(colon+1).trim();
+		String rightarr[] = right.split("\\|", -1);
+		for(int i=0; i<rightarr.length; i++)
+		{
+			String thisrule = rightarr[i].trim();
+			
+			
+		}
+		
+		return result;
+	}
+	
+	/*
+	 * Removes instances of left recursion from the grammar; 
+	 * described on Louden p 158 
+	 */
+	private HashMap<Nonterminal, ArrayList<Rule>> removeLeftRecursion(ArrayList<String> rawRules)
+	{	
+		ArrayList<Rule> rules = new ArrayList<Rule>();
+		for(String lineRule : rawRules)
+		{
+			ArrayList<Rule> rule = this.getRules(lineRule);
+			rules.addAll(rule);
+		}
+		
 		return null;
 	}
 	
 	// magical function that removes common prefixes
-	private ArrayList<String> removeCommonPrefix(ArrayList<String> rules)
+	private HashMap<Nonterminal, ArrayList<Rule>> removeCommonPrefix(HashMap<Nonterminal, ArrayList<Rule>> rules)
 	{
 		return null;
 	}
